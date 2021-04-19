cda862c77bf2521e223fdb3be6d74325a3950e98
Gav <gavinbruce12@googlemail.com>
2021-04-19 19:04:41 +0100

    Added DigitalClockPanel class

---
+++ src/DigitalClockPanel.java
@@ -0,0 +1,25 @@
+
+import java.awt.Graphics;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Gav
+ */
+public class DigitalClockPanel extends ClockPanel {
+    
+    public DigitalClockPanel(Model m) {
+        super(m);
+    }
+    
+    @Override
+    public void paintComponent(Graphics g) { 
+    
+    }
+    
+}
